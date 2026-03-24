9b8a4c2060510931291a0cdf5f5109805272ce7b
RonaldoGoatSii <172612901+RonaldoGoatSii@users.noreply.github.com>
2026-03-24 21:47:29 +0000

    feat: demo of the search bar changed, and added filters

--- search/views.py
+++ search/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
 
 def search_view(request):
+    query = request.GET.get('q')
+    print(query)  # só para testar
     return render(request, 'search/search.html')
