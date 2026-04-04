e25f6ecfedf06b569d48201c57590b952e2892cb
Dumitru Osieriu <78100242+CrisAxe@users.noreply.github.com>
2026-04-04 19:41:46 +0000

    fix:fixing joblisting views.py

--- jobListings/views.py
+++ jobListings/views.py
@@ -1,38 +1,32 @@
-from django.db.models import F
-from django.http import HttpResponseRedirect, HttpResponse
-from django.shortcuts import get_object_or_404, render, redirect
-from django.urls import reverse
-from django.views import generic
-from django.utils import timezone
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from .models import JobListing
 from .forms import JobForm
-from django.contrib.auth.decorators import login_required
+from company.models import Company
 
 def index(request):
     job_list = JobListing.objects.all()
     return render(request, "jobListings/index.html", {"job_list": job_list})
 
-
 def job_detail(request, pk):
     job = get_object_or_404(JobListing, pk=pk)
     return render(request, "jobListings/job_detail.html", {"job": job})
 
-
 @login_required
 def create_job_view(request):
+    if not request.user.is_employer:
+        return redirect("account", pk=request.user.pk)
+
+    company = Company.objects.get(owner=request.user)
+
     if request.method == "POST":
         form = JobForm(request.POST)
         if form.is_valid():
             job = form.save(commit=False)
-            job.owner = request.user
+            job.company = company
             job.save()
             return redirect("account", pk=request.user.pk)
     else:
         form = JobForm()
 
-    return render(request, "jobs/create_job.html", {"form": form})
-
-def home(request):
-    recent_jobs = JobListing.objects.order_by('-date_published')[:5]
-    return render(request, 'jobListings/home.html', {'recent_jobs': recent_jobs})
+    return render(request, "jobListings/create_job.html", {"form": form})
